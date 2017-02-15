652e8a1796f6b016ef4efb4f87c66b853ae0a351
Javier <Javier@1DAWProfe>
2017-02-15 08:55:50 +0100

    Uso de private

--- src/tresenraya/Tablero.java
+++ src/tresenraya/Tablero.java
@@ -2,41 +2,42 @@ package tresenraya;
 
 public class Tablero {
     
-    byte[][] tablero = new byte[3][3];
+    private byte[][] arrayTablero = new byte[3][3];
     
     public void inicializar() {
         // TODO: Hacer esto con bucles
-        tablero[0][0] = 0;
-        tablero[0][1] = 0;
-        tablero[0][2] = 0;
-        tablero[1][0] = 0;
-        tablero[1][1] = 0;
-        tablero[1][2] = 0;
-        tablero[2][0] = 0;
-        tablero[2][1] = 0;
-        tablero[2][2] = 0;
+        arrayTablero[0][0] = 0;
+        arrayTablero[0][1] = 0;
+        arrayTablero[0][2] = 0;
+        arrayTablero[1][0] = 0;
+        arrayTablero[1][1] = 0;
+        arrayTablero[1][2] = 0;
+        arrayTablero[2][0] = 0;
+        arrayTablero[2][1] = 0;
+        arrayTablero[2][2] = 0;
     }
     
-    public void mostrar() {
+    private void mostrar() {
         // TODO: Hacer esto con bucles
-        System.out.print(tablero[0][0]);
-        System.out.print(tablero[0][1]);
-        System.out.println(tablero[0][2]);
-        System.out.print(tablero[1][0]);
-        System.out.print(tablero[1][1]);
-        System.out.println(tablero[1][2]);
-        System.out.print(tablero[2][0]);
-        System.out.print(tablero[2][1]);
-        System.out.println(tablero[2][2]);
+        System.out.print(arrayTablero[0][0]);
+        System.out.print(arrayTablero[0][1]);
+        System.out.println(arrayTablero[0][2]);
+        System.out.print(arrayTablero[1][0]);
+        System.out.print(arrayTablero[1][1]);
+        System.out.println(arrayTablero[1][2]);
+        System.out.print(arrayTablero[2][0]);
+        System.out.print(arrayTablero[2][1]);
+        System.out.println(arrayTablero[2][2]);
     }
 
     public boolean ponerFicha(int x, int y, int jugador) {
         // Si hay una ficha en esa posición, retornar false
-        if(tablero[x][y] > 0 || x < 0 || x > 2 || y < 0 || y > 2) {
+        if(arrayTablero[x][y] > 0 || x < 0 || x > 2 || y < 0 || y > 2) {
             return false;
         } else {
             // Poner la ficha
-            tablero[x][y] = (byte)jugador;
+            arrayTablero[x][y] = (byte)jugador;
+            this.mostrar();
             return true;
         }
     }
